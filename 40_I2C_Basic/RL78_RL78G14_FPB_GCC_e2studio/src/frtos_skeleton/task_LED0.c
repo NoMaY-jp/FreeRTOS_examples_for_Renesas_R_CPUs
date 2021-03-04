de0711929fb37d219cae961c1eb3a450e9bac900
NoMaY (a user of Japan.RenesasRulz.com) <NoMaY-jp@outlook.com>
2021-03-04 23:14:48 +0900

    Add I2C_Basic RL78/G14 FPB example

---
+++ 40_I2C_Basic/RL78_RL78G14_FPB_GCC_e2studio/src/frtos_skeleton/task_LED0.c
@@ -0,0 +1,94 @@
+/***********************************************************************************************************************
+* DISCLAIMER
+* This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products. No
+* other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
+* applicable laws, including copyright laws.
+* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
+* THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED. TO THE MAXIMUM
+* EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES
+* SHALL BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS
+* SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability of
+* this software. By using this software, you agree to the additional terms and conditions found by accessing the
+* following link:
+* http://www.renesas.com/disclaimer
+*
+* Copyright (C) 2019 Renesas Electronics Corporation. All rights reserved.
+***********************************************************************************************************************/
+/***********************************************************************************************************************
+Includes   <System Includes> , "Project Includes"
+***********************************************************************************************************************/
+#include "task_function.h"
+/* Start user code for import. Do not edit comment generated here */
+#include "freertos_start.h"
+#include "platform.h"
+#include "r_cg_serial.h"
+#include "rl78g14fpbdef.h"
+#include "demo_slave.h"
+
+#if defined(RENESAS_SIMULATOR_DEBUGGING)
+/* Hardware or Renesas RL78 Simulator */
+#define LED0_BLINK_FREQUENCY_MS pdMS_TO_TICKS( !IsRenesasSimDebugMode() ? 1000 : 100 )
+#else
+/* Hardware only */
+#define LED0_BLINK_FREQUENCY_MS pdMS_TO_TICKS( 1000 )
+#endif
+/* End user code. Do not edit comment generated here */
+
+void task_LED0(void * pvParameters)
+{
+/* Start user code for function. Do not edit comment generated here */
+
+    uint8_t last_LED0_stat;
+    uint8_t send_buff[3];
+    MD_STATUS status;
+
+    INTERNAL_NOT_USED( pvParameters );
+
+    if (IsRenesasSimDebugMode())
+    {
+        vTaskDelay( portMAX_DELAY - 1 );
+    }
+
+    send_buff[0] = DEMO_SLAVE_SET_LED_STAT;
+    send_buff[1] = DEMO_SLAVE_LED0;
+    send_buff[2] = LED_ON;
+    U_IICA0_Master_Lock();
+    status = U_IICA0_Master_Send_Wait( DEMO_SLAVE_I2C_ADDR7, send_buff, 3 );
+    if (MD_OK != status)
+    {
+        configASSERT( ( volatile void * ) NULL );   /* Force an assert. */
+        for (;;)
+        {
+            nop();
+        }
+    }
+    U_IICA0_Master_Unlock();
+    last_LED0_stat = send_buff[2];
+
+    for (;;)
+    {
+        vTaskDelay( LED0_BLINK_FREQUENCY_MS );
+
+        send_buff[0] = DEMO_SLAVE_SET_LED_STAT;
+        send_buff[1] = DEMO_SLAVE_LED0;
+        send_buff[2] = (last_LED0_stat == LED_ON) ? LED_OFF : LED_ON;
+        U_IICA0_Master_Lock();
+        status = U_IICA0_Master_Send_Wait( DEMO_SLAVE_I2C_ADDR7, send_buff, 3 );
+        if (MD_OK != status)
+        {
+            configASSERT( ( volatile void * ) NULL );   /* Force an assert. */
+            for (;;)
+            {
+                nop();
+            }
+        }
+        U_IICA0_Master_Unlock();
+        last_LED0_stat = send_buff[2];
+    }
+
+/* End user code. Do not edit comment generated here */
+}
+/* Start user code for other. Do not edit comment generated here */
+/* End user code. Do not edit comment generated here */
---
+++ 40_I2C_Basic/RL78_RL78G14_FPB_GCC_e2studio/src/r_cg_vector_table.h
@@ -0,0 +1,228 @@
+#ifndef VECTOR_TABLE_H
+#define VECTOR_TABLE_H
+
+/* Defines for user */
+
+/*
+ * INT_IT (0x38 or 0x3C)
+ */
+#if INTIT_vect == 0x38
+
+#define R_Vector_0x38 vPortTickISR
+
+#elif INTIT_vect == 0x3C
+
+#define R_Vector_0x3C vPortTickISR
+
+#else
+
+#error Neither vector 0x38 nor vector 0x3C is available for the tick interrupt.
+
+#endif
+
+void vPortTickISR(void) __attribute__ ((interrupt));
+
+/*
+ * INT_BRK_I (0x7E)
+ */
+#define R_Vector_0x7E vPortYield
+
+void vPortYield(void) __attribute__ ((interrupt));
+
+/* Defines for default */
+
+#ifndef R_Vector_0x4
+#define R_Vector_0x4  R_Dummy
+#endif
+#ifndef R_Vector_0x6
+#define R_Vector_0x6  R_Dummy
+#endif
+#ifndef R_Vector_0x8
+#define R_Vector_0x8  R_Dummy
+#endif
+#ifndef R_Vector_0xA
+#define R_Vector_0xA  R_Dummy
+#endif
+#ifndef R_Vector_0xC
+#define R_Vector_0xC  R_Dummy
+#endif
+#ifndef R_Vector_0xE
+#define R_Vector_0xE  R_Dummy
+#endif
+
+#ifndef R_Vector_0x10
+#define R_Vector_0x10 R_Dummy
+#endif
+#ifndef R_Vector_0x12
+#define R_Vector_0x12 R_Dummy
+#endif
+#ifndef R_Vector_0x14
+#define R_Vector_0x14 R_Dummy
+#endif
+#ifndef R_Vector_0x16
+#define R_Vector_0x16 R_Dummy
+#endif
+#ifndef R_Vector_0x18
+#define R_Vector_0x18 R_Dummy
+#endif
+#ifndef R_Vector_0x1A
+#define R_Vector_0x1A R_Dummy
+#endif
+#ifndef R_Vector_0x1C
+#define R_Vector_0x1C R_Dummy
+#endif
+#ifndef R_Vector_0x1E
+#define R_Vector_0x1E R_Dummy
+#endif
+
+#ifndef R_Vector_0x20
+#define R_Vector_0x20 R_Dummy
+#endif
+#ifndef R_Vector_0x22
+#define R_Vector_0x22 R_Dummy
+#endif
+#ifndef R_Vector_0x24
+#define R_Vector_0x24 R_Dummy
+#endif
+#ifndef R_Vector_0x26
+#define R_Vector_0x26 R_Dummy
+#endif
+#ifndef R_Vector_0x28
+#define R_Vector_0x28 R_Dummy
+#endif
+#ifndef R_Vector_0x2A
+#define R_Vector_0x2A R_Dummy
+#endif
+#ifndef R_Vector_0x2C
+#define R_Vector_0x2C R_Dummy
+#endif
+#ifndef R_Vector_0x2E
+#define R_Vector_0x2E R_Dummy
+#endif
+
+#ifndef R_Vector_0x30
+#define R_Vector_0x30 R_Dummy
+#endif
+#ifndef R_Vector_0x32
+#define R_Vector_0x32 R_Dummy
+#endif
+#ifndef R_Vector_0x34
+#define R_Vector_0x34 R_Dummy
+#endif
+#ifndef R_Vector_0x36
+#define R_Vector_0x36 R_Dummy
+#endif
+#ifndef R_Vector_0x38
+#define R_Vector_0x38 R_Dummy
+#endif
+#ifndef R_Vector_0x3A
+#define R_Vector_0x3A R_Dummy
+#endif
+#ifndef R_Vector_0x3C
+#define R_Vector_0x3C R_Dummy
+#endif
+#ifndef R_Vector_0x3E
+#define R_Vector_0x3E R_Dummy
+#endif
+
+#ifndef R_Vector_0x40
+#define R_Vector_0x40 R_Dummy
+#endif
+#ifndef R_Vector_0x42
+#define R_Vector_0x42 R_Dummy
+#endif
+#ifndef R_Vector_0x44
+#define R_Vector_0x44 R_Dummy
+#endif
+#ifndef R_Vector_0x46
+#define R_Vector_0x46 R_Dummy
+#endif
+#ifndef R_Vector_0x48
+#define R_Vector_0x48 R_Dummy
+#endif
+#ifndef R_Vector_0x4A
+#define R_Vector_0x4A R_Dummy
+#endif
+#ifndef R_Vector_0x4C
+#define R_Vector_0x4C R_Dummy
+#endif
+#ifndef R_Vector_0x4E
+#define R_Vector_0x4E R_Dummy
+#endif
+
+#ifndef R_Vector_0x50
+#define R_Vector_0x50 R_Dummy
+#endif
+#ifndef R_Vector_0x52
+#define R_Vector_0x52 R_Dummy
+#endif
+#ifndef R_Vector_0x54
+#define R_Vector_0x54 R_Dummy
+#endif
+#ifndef R_Vector_0x56
+#define R_Vector_0x56 R_Dummy
+#endif
+#ifndef R_Vector_0x58
+#define R_Vector_0x58 R_Dummy
+#endif
+#ifndef R_Vector_0x5A
+#define R_Vector_0x5A R_Dummy
+#endif
+#ifndef R_Vector_0x5C
+#define R_Vector_0x5C R_Dummy
+#endif
+#ifndef R_Vector_0x5E
+#define R_Vector_0x5E R_Dummy
+#endif
+
+#ifndef R_Vector_0x60
+#define R_Vector_0x60 R_Dummy
+#endif
+#ifndef R_Vector_0x62
+#define R_Vector_0x62 R_Dummy
+#endif
+#ifndef R_Vector_0x64
+#define R_Vector_0x64 R_Dummy
+#endif
+#ifndef R_Vector_0x66
+#define R_Vector_0x66 R_Dummy
+#endif
+#ifndef R_Vector_0x68
+#define R_Vector_0x68 R_Dummy
+#endif
+#ifndef R_Vector_0x6A
+#define R_Vector_0x6A R_Dummy
+#endif
+#ifndef R_Vector_0x6C
+#define R_Vector_0x6C R_Dummy
+#endif
+#ifndef R_Vector_0x6E
+#define R_Vector_0x6E R_Dummy
+#endif
+
+#ifndef R_Vector_0x70
+#define R_Vector_0x70 R_Dummy
+#endif
+#ifndef R_Vector_0x72
+#define R_Vector_0x72 R_Dummy
+#endif
+#ifndef R_Vector_0x74
+#define R_Vector_0x74 R_Dummy
+#endif
+#ifndef R_Vector_0x76
+#define R_Vector_0x76 R_Dummy
+#endif
+#ifndef R_Vector_0x78
+#define R_Vector_0x78 R_Dummy
+#endif
+#ifndef R_Vector_0x7A
+#define R_Vector_0x7A R_Dummy
+#endif
+#ifndef R_Vector_0x7C
+#define R_Vector_0x7C R_Dummy
+#endif
+#ifndef R_Vector_0x7E
+#define R_Vector_0x7E R_Dummy
+#endif
+
+#endif
