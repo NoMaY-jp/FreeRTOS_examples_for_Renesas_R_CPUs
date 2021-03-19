f1f75a3bc837f01cf48a37ba1eda547966a17226
NoMaY (a user of Japan.RenesasRulz.com) <NoMaY-jp@outlook.com>
2021-03-19 21:34:11 +0900

    Minor update of RL78/G14 FPB examples: I2C_Simple_Master_100Kbps_Advanced, I2C_100Kbps_Advanced

--- 31_I2C_Simple_Master_100Kbps_Advanced/RL78_RL78G14_FPB_IAR_e2studio_EWRL78/src/frtos_startup/freertos_stack.h
+++ 31_I2C_Simple_Master_100Kbps_Advanced/RL78_RL78G14_FPB_IAR_e2studio_EWRL78/src/frtos_startup/freertos_stack.h
@@ -14,25 +14,25 @@ extern "C" {
 #if defined(__CCRL__)
 
 /* The stack usage analysis tool 'Call Walker' provides detail stack usage information.
- * The following table is based on the result of analysis 12-Mar-2021.
+ * The following table is based on the result of analysis 19-Mar-2021.
  *
  * The stack space for interrupt stub is calculated as follows:
  *
- * interrupt stub size = MAX( space for task context, space for nested interrupt, TM3 interrupt )
- *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), TM3 interrupt )
- *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), TM3 interrupt )
- *                     = MAX( 16, 14, TM3 interrupt )
+ * interrupt stub size = MAX( space for task context, space for nested interrupt, MAX(TM3, IIC00) interrupt )
+ *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), MAX(TM3, IIC00) interrupt )
+ *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), MAX(TM3, IIC00) interrupt )
+ *                     = MAX( 16, 14, MAX(TM3, IIC00) interrupt )
  */
 
 #if defined(NDEBUG)
 
 /* The following table is for the case of using compile optimization of CC-RL. */
 
-/* The stack space for TM3 interrupt is 20 */
+/* The stack space for TM3 & IIC00 interrupt is MAX(20, 4) = 20 */
 
 #define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  62, 20, 32 ) )
 #define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  64, 20, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  60, 20, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  56, 20, 32 ) )
 #define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 20, 32 ) )
 #define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 20, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
@@ -41,11 +41,11 @@ extern "C" {
 
 /* The following table is for the case of NOT using compile optimization of CC-RL. */
 
-/* The stack space for TM3 interrupt is 36 */
+/* The stack space for TM3 & IIC00 interrupt is MAX(36, 4) = 36 */
 
 #define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES( 106, 36, 32 ) )
 #define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES( 108, 36, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  88, 36, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  82, 36, 32 ) )
 #define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   6, 36, 32 ) )
 #define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(  10, 36, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
@@ -55,25 +55,25 @@ extern "C" {
 #elif defined(__ICCRL78__)
 
 /* The linker log file's stack usage call graph provides detail stack usage information.
- * The following table is based on the result of analysis 12-Mar-2021.
+ * The following table is based on the result of analysis 19-Mar-2021.
  *
  * The stack space for interrupt stub is calculated as follows:
  *
- * interrupt stub size = MAX( space for task context, space for nested interrupt, TM3 interrupt )
- *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), TM3 interrupt )
- *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), TM3 interrupt )
- *                     = MAX( 16, 14, TM3 interrupt )
+ * interrupt stub size = MAX( space for task context, space for nested interrupt, MAX(TM3, IIC00) interrupt )
+ *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), MAX(TM3, IIC00) interrupt )
+ *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), MAX(TM3, IIC00) interrupt )
+ *                     = MAX( 16, 14, MAX(TM3, IIC00) interrupt )
  */
 
 #if defined(NDEBUG)
 
 /* The following table is for the case of using compile optimization of ICCRL78. */
 
-/* The stack space for TM3 interrupt is 24 */
+/* The stack space for TM3 & IIC00 interrupt is MAX(24, 4) = is 24 */
 
-#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  72, 24, 32 ) )
-#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  74, 24, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  66, 24, 32 ) )
+#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  70, 24, 32 ) )
+#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  72, 24, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  64, 24, 32 ) )
 #define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 24, 32 ) )
 #define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 24, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
@@ -82,11 +82,11 @@ extern "C" {
 
 /* The following table is for the case of NOT using compile optimization of ICCRL78. */
 
-/* The stack space for TM3 interrupt is 30 */
+/* The stack space for TM3 & IIC00 interrupt is MAX(30, 4) = 30 */
 
 #define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  90, 30, 32 ) )
 #define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  92, 30, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  72, 30, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  66, 30, 32 ) )
 #define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 30, 32 ) )
 #define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(  10, 30, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
--- 41_I2C_100Kbps_Advanced/RL78_RL78G14_FPB_Renesas_e2studio_CS+/src/frtos_startup/freertos_stack.h
+++ 41_I2C_100Kbps_Advanced/RL78_RL78G14_FPB_Renesas_e2studio_CS+/src/frtos_startup/freertos_stack.h
@@ -14,25 +14,25 @@ extern "C" {
 #if defined(__CCRL__)
 
 /* The stack usage analysis tool 'Call Walker' provides detail stack usage information.
- * The following table is based on the result of analysis 12-Mar-2021.
+ * The following table is based on the result of analysis 19-Mar-2021.
  *
  * The stack space for interrupt stub is calculated as follows:
  *
- * interrupt stub size = MAX( space for task context, space for nested interrupt, TM3 interrupt )
- *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), TM3 interrupt )
- *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), TM3 interrupt )
- *                     = MAX( 16, 14, TM3 interrupt )
+ * interrupt stub size = MAX( space for task context, space for nested interrupt, IICA0 interrupt )
+ *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), IICA0 interrupt )
+ *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), IICA0 interrupt )
+ *                     = MAX( 16, 14, IICA0 interrupt )
  */
 
 #if defined(NDEBUG)
 
 /* The following table is for the case of using compile optimization of CC-RL. */
 
-/* The stack space for TM3 interrupt is 20 */
+/* The stack space for IICA0 interrupt is 20 */
 
 #define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  62, 20, 32 ) )
 #define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  64, 20, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  60, 20, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  56, 20, 32 ) )
 #define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 20, 32 ) )
 #define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 20, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
@@ -41,13 +41,13 @@ extern "C" {
 
 /* The following table is for the case of NOT using compile optimization of CC-RL. */
 
-/* The stack space for TM3 interrupt is 36 */
+/* The stack space for IICA0 interrupt is 34 */
 
-#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES( 106, 36, 32 ) )
-#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES( 108, 36, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  88, 36, 32 ) )
-#define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   6, 36, 32 ) )
-#define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(  10, 36, 32 ) )
+#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES( 106, 34, 32 ) )
+#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES( 108, 34, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  82, 34, 32 ) )
+#define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   6, 34, 32 ) )
+#define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(  10, 34, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
 
 #endif
@@ -55,40 +55,40 @@ extern "C" {
 #elif defined(__ICCRL78__)
 
 /* The linker log file's stack usage call graph provides detail stack usage information.
- * The following table is based on the result of analysis 12-Mar-2021.
+ * The following table is based on the result of analysis 19-Mar-2021.
  *
  * The stack space for interrupt stub is calculated as follows:
  *
- * interrupt stub size = MAX( space for task context, space for nested interrupt, TM3 interrupt )
- *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), TM3 interrupt )
- *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), TM3 interrupt )
- *                     = MAX( 16, 14, TM3 interrupt )
+ * interrupt stub size = MAX( space for task context, space for nested interrupt, IICA0 interrupt )
+ *                     = MAX( (PC + PSW + REGS + usCriticalNesting), (PC + PSW + REGS), IICA0 interrupt )
+ *                     = MAX( (3 + 1 + 10 + 2), (3 + 1 + 10), IICA0 interrupt )
+ *                     = MAX( 16, 14, IICA0 interrupt )
  */
 
 #if defined(NDEBUG)
 
 /* The following table is for the case of using compile optimization of ICCRL78. */
 
-/* The stack space for TM3 interrupt is 24 */
+/* The stack space for IICA0 interrupt is 22 */
 
-#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  72, 24, 32 ) )
-#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  74, 24, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  66, 24, 32 ) )
-#define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 24, 32 ) )
-#define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 24, 32 ) )
+#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  72, 22, 32 ) )
+#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  74, 22, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  64, 22, 32 ) )
+#define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 22, 32 ) )
+#define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 22, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
 
 #else
 
 /* The following table is for the case of NOT using compile optimization of ICCRL78. */
 
-/* The stack space for TM3 interrupt is 30 */
+/* The stack space for IICA0 interrupt is 28 */
 
-#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  90, 30, 32 ) )
-#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  92, 30, 32 ) )
-#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  72, 30, 32 ) )
-#define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 30, 32 ) )
-#define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(  10, 30, 32 ) )
+#define task_LED0_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  90, 28, 32 ) )
+#define task_LED1_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(  92, 28, 32 ) )
+#define task_DMSLV_STACK_BUFF_DEPTH stackBYTES_TO_DEPTH( stackBUFF_BYTES(  66, 28, 32 ) )
+#define main_task_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   4, 28, 32 ) )
+#define IdleTask_STACK_BUFF_DEPTH   stackBYTES_TO_DEPTH( stackBUFF_BYTES(  10, 28, 32 ) )
 #define TimerTask_STACK_BUFF_DEPTH  stackBYTES_TO_DEPTH( stackBUFF_BYTES(   0,  0,  0 ) )
 
 #endif
